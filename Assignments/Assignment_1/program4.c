bbe1b0324722d31d7380249a51b74d036f76d2d2
TejalSanjayShinde <shindetejal567@gmail.com>
2025-11-03 12:55:39 +0530

    Add files via upload
    Add Assignment_1 solution (Q1 - Q5)

---
+++ Assignments/Assignment_1/program4.c
@@ -0,0 +1,37 @@
+//Accept one number and check whether is divisible by 5 or not.
+
+#include<stdio.h>
+#include<stdbool.h>
+
+CheckNumber(int iNo)
+{
+    if((iNo % 5) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+    printf("Enter number");
+    scanf("%d",&iValue);
+
+    bRet = CheckNumber(iValue);
+
+    if(bRet == true)
+    {
+        printf("Divisible by 5");
+    }
+    else
+    {
+        printf("Not Divisible by 5");
+    }
+
+    return 0;
+}
